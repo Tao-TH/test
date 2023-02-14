1d6afe9a69697e856b0e96b0ae2cea9432f962c4
Tao <2543987732@qq.com>
2023-02-14 01:18:46 -0800

    Commit message

---
+++ bash/run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+CODE_DIR=/home/tao/Desktop/src/bash
+cd $CODE_DIR
+#编写镜像文件
+cat >./Dockerfile << a
+FROM alpine
+RUN echo 构建时运行
+CMD echo hello world
+a
+# 构建镜像
+cd $CODE_DIR && docker build -t test2:v1 .
+# 在容器中运行应用程序
+docker run test2:v1
+
