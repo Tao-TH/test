033756195527f9a221f25c3493c27cebcd68b7ca
Tao-TH <107127414+Tao-TH@users.noreply.github.com>
2023-02-14 22:49:15 +0800

    Update run.sh

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-CODE_DIR=/.github/workflows
+
 cd $CODE_DIR
 #编写镜像文件
 cat >./Dockerfile << a
@@ -8,7 +8,7 @@ RUN echo 构建时运行
 CMD echo hello world
 a
 # 构建镜像
-cd $CODE_DIR && docker build -t test2:v1 .
+ docker build -t test2:v1 .
 # 在容器中运行应用程序
 docker run test2:v1
 
